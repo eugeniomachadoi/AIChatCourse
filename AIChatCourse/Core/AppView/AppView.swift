3166853e216d9467c01afe924aef29449e18c8e7
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-05-19 20:56:08 +0100

    Set appview and builder

---
+++ AIChatCourse/Core/AppView/AppView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct AppView: View {
+    @AppStorage("showTabbarView") var showTabBar: Bool = false
+
+    var body: some View {
+        AppViewBuilder(
+            showTabBar: showTabBar,
+            tabbarView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Tabbar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+    }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(showTabBar: false)
+}
+
+#Preview("AppView - Onboarding") {
+    AppView(showTabBar: true)
+}
