45f63cad271ceef319489549a0dde0d4e3e6ad7e
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-05-20 20:48:54 +0100

    Welcome view

--- AIChatCourse/Core/Welcome/WelcomeView.swift
+++ AIChatCourse/Core/Welcome/WelcomeView.swift
@@ -1,20 +1,69 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State private var imageName: String = Constants.randomImage
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
+            VStack(spacing: 8) {
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
 
-                NavigationLink {
-                   OnboardingCompletedView()
-                } label: {
-                    Text("Get started")
-                        .callToActionButton()
-                }
+                titleSection
+                    .padding(.top, 24)
+
+                ctaButtons
+                    .padding(16)
+
+                policyLinks
             }
-            .padding(16)
+        }
+    }
+
+    private var titleSection: some View {
+        VStack {
+            Text("AI Chat 🤙")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+
+            Text("Youtube @ SwitfulThinking")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+
+    private var ctaButtons: some View {
+        VStack(spacing: 8) {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get started")
+                    .callToActionButton()
+            }
+
+            Text("Already have an account? Sign in!")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .tappableBackground()
+                .onTapGesture {
+
+                }
+        }
+    }
+
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termosOfServiceURL)!) {
+                Text("Terms of Service")
+            }
+
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+
+            Link(destination: URL(string: Constants.privacyPolicyURL)!) {
+                Text("Privacy Policy")
+            }
         }
     }
 }
---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicyURL = "https://www.apple.com"
+    static let termosOfServiceURL = "https://www.apple.com"
+}
