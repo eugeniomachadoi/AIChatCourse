cf76be7549a10c8852b49fbbf2f9c2ce5805050a
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-05-23 14:10:52 +0100

    Onboarding completed view

---
+++ AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChatCourse
+//
+//  Created by Eugênio Inocêncio on 21/05/2025.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
---
+++ AIChatCourse/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatCourse
+//
+//  Created by Eugênio Inocêncio on 21/05/2025.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
