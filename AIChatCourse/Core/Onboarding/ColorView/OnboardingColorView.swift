d084eec9e2c913dc7abc2b042522c11be412ea58
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-05-23 16:57:04 +0100

    Hero cell view

--- AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
+++ AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -1,18 +1,67 @@
-//
-//  OnboardingColorView.swift
-//  AIChatCourse
-//
-//  Created by Eugênio Inocêncio on 21/05/2025.
-//
-
 import SwiftUI
 
 struct OnboardingColorView: View {
+    @State private var selectedColor: Color?
+
+    let profileColors: [Color] = [.red, .green, .orange, .blue, .mint, .purple, .cyan, .teal, .indigo]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            colorGrid
+                .padding(.horizontal, 24)
+        }
+        .safeAreaInset(edge: .bottom, alignment: .center, spacing: 16, content: {
+            ZStack {
+                if let selectedColor {
+                    ctaButton(selectedColor: selectedColor)
+                        .transition(AnyTransition.move(edge: .bottom))
+                }
+            }
+            .padding(24)
+            .background(Color(uiColor: .systemBackground))
+        })
+        .animation(.smooth, value: selectedColor)
+    }
+
+    private var colorGrid: some View {
+        LazyVGrid(
+            columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+            alignment: .center,
+            spacing: 16,
+            pinnedViews: [.sectionHeaders],
+            content: {
+                Section(content: {
+                    ForEach(profileColors, id: \.self) { color in
+                        Circle()
+                            .fill(.accent)
+                            .overlay(
+                                color
+                                    .clipShape(Circle())
+                                    .padding(selectedColor == color ? 10 : 0)
+                            )
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
+                }, header: {
+                    Text("Select a profile color")
+                        .font(.headline)
+                })
+            })
+    }
+
+    private func ctaButton(selectedColor: Color) -> some View {
+        NavigationLink {
+            OnboardingCompletedView(selectedColor: selectedColor)
+        } label: {
+            Text("Continue")
+                .callToActionButton()
+        }
     }
 }
 
 #Preview {
-    OnboardingColorView()
+    NavigationStack {
+        OnboardingColorView()
+    }
+    .environment(AppState())
 }
