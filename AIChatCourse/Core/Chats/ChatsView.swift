2b64d8e2fb9a4fe86ad2f6e6f95ab3790f4ab096
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-05-26 18:28:18 +0100

    chat row cell view

--- AIChatCourse/Core/Chats/ChatsView.swift
+++ AIChatCourse/Core/Chats/ChatsView.swift
@@ -7,7 +7,22 @@ struct ChatsView: View {
         NavigationStack {
             List {
                 ForEach(chats) { chat in
-                    Text(chat.id)
+                    ChatRowCellViewBuilder(
+                        chat: chat,
+                        currentUserId: nil, // Add cuid,
+                        getAvatar: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return .mock
+                        },
+                        getLastChatMessage: {
+                            try? await Task.sleep(for: .seconds(1))
+                            return .mock
+                        }
+                    )
+                    .anyButton(.highlight, action: {
+                        
+                    })
+                    .removeListRowFormatting()
                 }
             }
             .navigationTitle("Chats")
