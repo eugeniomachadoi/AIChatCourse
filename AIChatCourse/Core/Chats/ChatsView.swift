6a193eafb78d7910221c85aa9826e6648e48fea8
Eugênio Inocêncio <eugenio.inocencio@infraspeak.com>
2025-06-11 17:15:20 +0100

    recent avatars

--- AIChatCourse/Core/Chats/ChatsView.swift
+++ AIChatCourse/Core/Chats/ChatsView.swift
@@ -2,15 +2,37 @@ import SwiftUI
 
 struct ChatsView: View {
     @State private var chats: [ChatModel] = ChatModel.mocks
+    @State private var recentAvatars: [AvatarModel] = AvatarModel.mocks
     @State private var path: [NavigationPathOption] = []
 
     var body: some View {
         NavigationStack(path: $path) {
             List {
+                if !recentAvatars.isEmpty {
+                    recentsSection
+                }
+                chatsSection
+            }
+            .navigationTitle("Chats")
+            .navigationDestinationForCoreModule(path: $path)
+        }
+    }
+
+    private var chatsSection: some View {
+        Section {
+            if chats.isEmpty {
+                Text("Your chats will appear here!")
+                    .foregroundStyle(.secondary)
+                    .font(.title3)
+                    .frame(maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .padding(40)
+                    .removeListRowFormatting()
+            } else {
                 ForEach(chats) { chat in
                     ChatRowCellViewBuilder(
                         chat: chat,
-                        currentUserId: nil, // Add cuid,
+                        currentUserId: nil,  // Add cuid,
                         getAvatar: {
                             try? await Task.sleep(for: .seconds(1))
                             return AvatarModel.mocks.randomElement()!
@@ -20,19 +42,57 @@ struct ChatsView: View {
                             return ChatMessageModel.mocks.randomElement()!
                         }
                     )
-                    .anyButton(.highlight, action: {
-                        onChatPressed(chat: chat)
-                    })
+                    .anyButton(
+                        .highlight,
+                        action: {
+                            onChatPressed(chat: chat)
+                        }
+                    )
                     .removeListRowFormatting()
                 }
             }
-            .navigationTitle("Chats")
-            .navigationDestinationForCoreModule(path: $path)
+        } header: {
+            Text("Chats")
+        }
+    }
+
+    private var recentsSection: some View {
+        Section {
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 8) {
+                    ForEach(recentAvatars, id: \.self) { avatar in
+                        if let imageName = avatar.profileImageName {
+                            VStack(spacing: 8) {
+                                ImageLoaderView(urlString: imageName)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .clipShape(Circle())
+
+                                Text(avatar.name ?? "")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            .anyButton {
+                                onAvatarPressed(avatar: avatar)
+                            }
+                        }
+                    }
+                }
+                .padding(.top, 12)
+            }
+            .frame(height: 120)
+            .scrollIndicators(.hidden)
+            .removeListRowFormatting()
+        } header: {
+            Text("Recents")
         }
     }
 
     private func onChatPressed(chat: ChatModel) {
         path.append(.chat(avatarId: chat.avatarId))
+    }
+
+    private func onAvatarPressed(avatar: AvatarModel) {
+        path.append(.chat(avatarId: avatar.avatarId))
     }
 }
 
